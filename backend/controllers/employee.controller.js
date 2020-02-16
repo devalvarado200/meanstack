96dbea5de012507b729bc8a4dbc6512c09f80f5c
Victor <victoraav2017@gmail.com>
2020-02-16 00:19:18 -0430

    backend done

--- backend/controllers/employee.controller.js
+++ backend/controllers/employee.controller.js
@@ -4,7 +4,9 @@ const Employee = require("../models/Employee.model");
 const employeeController = {};
 
 employeeController.getEmployee =  async (req, res) =>{
-	const employee = await Employee.find();
+	const { id }= req.params;
+	const employee = await Employee.findById(id);
+
 	res.json(employee);
 };
 
@@ -14,15 +16,30 @@ employeeController.getEmployees =  async (req, res) =>{
 };
 
 employeeController.createEmployee = async (req, res) =>{
-	
+	const employee = new Employee(req.body);
+	await employee.save();
+	res.json({status: 200})
 };
 
 employeeController.editEmployee = async (req, res) =>{
-	
+	const { id }= req.params;
+	const employee = {
+		name: req.body.name,
+		position: req.body.position,
+		office: req.body.office,
+		salary: req.body.salary
+	}
+	await Employee.findByIdAndUpdate(id, 
+		{$set: employee},
+		{new: true}
+	);
+	res.json({status: 200})
 };
 
 employeeController.deleteEmployee = async (req, res) =>{
-	
+	const { id }= req.params;
+	await Employee.findByIdAndRemove(id);
+	res.json({status: 200})
 };
 
 
